151ca93ba5986739fd109d86316bf99d85879e0a
furkanyelbey <leona.vermillion@outlook.com>
2025-02-12 13:09:22 +0300

    swagger added

--- baykariha/urls.py
+++ baykariha/urls.py
@@ -16,8 +16,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Baykar API Dokümantasyonu",
+        default_version='v1',
+        description="Baykar test uygulaması için API dökümantasyonu",
+    ),
+    public=True,
+)
 
 urlpatterns = [
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('admin/', admin.site.urls),
     path('', include('baykartryout.urls')),
     path('api/', include('baykartryout.api.urls')),
